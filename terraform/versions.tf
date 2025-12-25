9eefb5758c9fda2bf6ebded116cece47b8f2ae20
Joerg Jung <joerg-jung@gmx.de>
2025-12-25 22:56:16 +0100

    Initial commit: Digital Twin infrastructure and application

---
+++ terraform/versions.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_version = ">= 1.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+
+provider "aws" {
+  # Uses AWS CLI configuration (aws configure)
+}
+
+provider "aws" {
+  alias  = "us_east_1"
+  region = "us-east-1"
+  #alias  = "eu_central_1"
+  #region = "eu-central-1"
+}
